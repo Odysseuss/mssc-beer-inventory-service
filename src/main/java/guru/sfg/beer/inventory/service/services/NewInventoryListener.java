ba55543ba2621bf870c8b70ebe1b4ff4a50021d9
Michael Ortiz <12751082+Odysseuss@users.noreply.github.com>
2023-03-14 00:06:57 -0400

    challenge new inventory listener

---
+++ src/main/java/guru/sfg/beer/inventory/service/services/NewInventoryListener.java
@@ -0,0 +1,35 @@
+package guru.sfg.beer.inventory.service.services;
+
+import guru.sfg.beer.inventory.service.config.JmsConfig;
+import guru.sfg.beer.inventory.service.domain.BeerInventory;
+import guru.sfg.beer.inventory.service.repositories.BeerInventoryRepository;
+import guru.sfg.common.events.NewInventoryEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class NewInventoryListener {
+
+    private final BeerInventoryRepository beerInventoryRepository;
+    private final JmsTemplate jmsTemplate;
+
+    @Transactional
+    @JmsListener(destination = JmsConfig.NEW_INVENTORY_QUEUE)
+    public void listen(NewInventoryEvent event) {
+
+        log.debug("Got Inventory: " + event.toString());
+
+        beerInventoryRepository.save(BeerInventory.builder()
+                .beerId(event.getBeerDto().getId())
+                .upc(event.getBeerDto().getUpc())
+                .quantityOnHand(event.getBeerDto().getQuantityOnHand())
+                .build());
+
+    }
+}
